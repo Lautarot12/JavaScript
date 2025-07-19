27470ab6499fb11ce403f0403842e1519e94307a
Lautaro Tello <lautaro_t11@hotmail.com>
2025-07-19 14:47:55 -0300

    enlazado de JS a HTML y creacion de variables

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+let nombre = "Lautaro";
+let edad = 23;
+const nacimiento = 2002;
+
+let esEstudiante = true;
+let esTrabajador = false;
+
+console.log(nombre);
+console.log(edad);
+console.log(nacimiento);
+console.log(esEstudiante);
+console.log(esTrabajador);
+
